88b00dc3bb1f3862f50fdc01f4b0acecc9831db0
Muhammed Furkan Aymergen <faymergen98@gmail.com>
2022-07-26 20:37:37 +0300

    turkish to english basic translator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 function App() {
-  return (
-    
-    <div className='card'>
-      <div className='card-header text-center'>aslkdsuajj</div>
-    </div>
+  
+    const [word,setWord] = useState()
+    const [result,setResult] = useState()
+
+     const getLang = async () =>{
+      if(word)
+      {
+        axios.post("https://libretranslate.de/translate",{
+          "q":word,
+          "source":"tr",
+          "target":"en"
+        }).then(response =>{
+          setResult(response.data.translatedText)
+        })
+      }
+      else
+      {
+        setResult("")
+      }
+      
+    }
+
+    return (
+      <div>
+          <div className='text-4xl my-20 text-center'>Turkish to English Word Translator</div>
+
+              <input type="text" className="p-3 block mx-auto my-10 bg-slate-600" value={word} onChange={(e) =>setWord(e.target.value)}/> 
+              <button onClick={getLang} className="block mx-auto  p-3 bg-slate-400 rounded-lg hover:bg-slate-600 duration-200 hover:cursor-pointer active:bg-slate-700 active:scale-90">Translate</button>
+              <p className="text-3xl my-10 text-center">{result}</p>
+      </div>
   );
 }
 
